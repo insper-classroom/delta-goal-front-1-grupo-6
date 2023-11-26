64304beb5bf37ff818931140e99b137ed1ad5e1e
Andre Pereira <deco9871@gmail.com>
2023-11-26 14:23:00 -0300

    feat: restart pwd

--- pages/login/script.js
+++ pages/login/script.js
@@ -21,6 +21,25 @@ async function login() {
     localStorage.setItem("token", responseData.jwt_token)
 
     window.location.href = "/dashboard.html"
+}
+
+async function forgotPwd() {
+    let email = document.querySelector("#email").value
+
+    if (!email) {
+        return toast("Preencha seu email", "error")
+    }
+
+    let responseData = await (await fetch("http://127.0.0.1:5500/email/send", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({email: email})
+    })).json()
+
+    toast(responseData.message, responseData.status)
+    
 
 
 }
