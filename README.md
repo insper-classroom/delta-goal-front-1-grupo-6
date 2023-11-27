2230aeb83b5f046367e2f411765bd9785235f60c
Andre Pereira <deco9871@gmail.com>
2023-11-26 22:10:37 -0300

    fix: reset pwd script -> forgot to pass token

--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 - https://stackoverflow.com/questions/14249506/how-can-i-wait-in-node-js-javascript-l-need-to-pause-for-a-period-of-time
 - https://www.w3schools.com/howto/howto_css_custom_scrollbar.asp
 - https://www.w3schools.com/howto/howto_css_hide_arrow_number.asp
+- https://stackoverflow.com/questions/901115/how-can-i-get-query-string-values-in-javascript
--- pages/resetPassword/script.js
+++ pages/resetPassword/script.js
@@ -5,12 +5,16 @@ async function resetPwd() {
         return toast("Preencha sua nova senha", "error")
     }
 
+    const urlParams = new URLSearchParams(window.location.search);
+    const token = urlParams.get('token');
+
+
     let responseData = await (await fetch("http://127.0.0.1:5500/email/send", {
         method: "POST",
         headers: {
             "Content-Type": "application/json"
         },
-        body: JSON.stringify({email: email})
+        body: JSON.stringify({email: email, token})
     })).json()
 
     toast(responseData.message, responseData.status)
