bbf389c7aa3d5a83495f79e3e367258fd59e95b1
Andre Pereira <deco9871@gmail.com>
2023-11-29 11:11:50 -0300

    feat: getGames function

--- pages/selectGame/script.js
+++ pages/selectGame/script.js
@@ -4,3 +4,16 @@ function logout() {
     localStorage.removeItem("token")
     window.location.href = "/pages/login/login.html"
 }
+
+async function getGames() {
+    let options = {
+        method: "GET",
+        headers: {"Authorization": "Bearer " + localStorage.getItem("token")}
+    }
+
+    let responseData = await (await fetch("http://127.0.0.1:5500/matches", options)).json()
+
+    console.log(responseData)
+}
+
+getGames()
