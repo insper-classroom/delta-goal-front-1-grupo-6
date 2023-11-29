7cd11fb9ffdbca9c207fa2bf03d6b16b6fcc53dd
Ana Helena <anahc3@al.insper.edu.br>
2023-11-29 11:36:57 -0300

    feat: logout + club name dash

---
+++ pages/crossingDashboard/script.js
@@ -0,0 +1,6 @@
+document.querySelector(".username").textContent = `Olá, ${localStorage.getItem("club_name")}`
+
+function logout() {
+    localStorage.removeItem("token")
+    window.location.href = "/pages/login/login.html"
+}
