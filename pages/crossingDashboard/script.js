ffc572fdb4379c9cc7fe3e4dd62eac02217fcad8
Ana Helena <anahc3@al.insper.edu.br>
2023-11-29 16:44:36 -0300

    feat: mudança de página html + css

--- pages/crossingDashboard/script.js
+++ pages/crossingDashboard/script.js
@@ -4,3 +4,7 @@ function logout() {
     localStorage.removeItem("token")
     window.location.href = "/pages/login/login.html"
 }
+
+function defensiveDash() {
+    window.location.href = "/pages/defensiveBreakthrough/defensiveBreakthrough.html"
+}
