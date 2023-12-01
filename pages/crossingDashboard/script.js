8fa196f19ef8f6f2925977449390f25eab3f9375
DECO190 <deco9871@gmail.com>
2023-12-01 11:49:44 -0300

    dev: render team 1 chart

--- pages/crossingDashboard/script.js
+++ pages/crossingDashboard/script.js
@@ -28,6 +28,35 @@ function drawChart(data, elementId) {
     chart.draw(data, options);
 }
 
+function getOutcomeChartData(data) {
+    let stats = [
+        ["Perdido", 0],
+        ["Bem-Sucedido", 0],
+        ["Bloqueado", 0],
+    ]
+    
+    for (let i in data) {
+        if (data[i].desfecho == "Perdido") {
+            stats[0][1] = stats[0][1] + 1
+        } else if (data[i].desfecho == "Bem-Sucedido") {
+            stats[1][1] = stats[1][1] + 1
+        } else if (data[i].desfecho == "Bloqueado") {
+            stats[2][1] = stats[2][1] + 1
+        }
+    }
+    
+    return stats
+}
+
+function renderOutcomeCharts(data) {
+    let team1ChartData = google.visualization.arrayToDataTable([["Desfecho", "Quantidade"], ...getOutcomeChartData(data.match.json_cruzamento.time[Object.keys(data.match.json_cruzamento.time)[0]].rupturas)])
+    let team2ChartData = google.visualization.arrayToDataTable([["Desfecho", "Quantidade"], ...getOutcomeChartData(data.match.json_cruzamento.time[Object.keys(data.match.json_cruzamento.time)[1]].rupturas)])
+
+    let chart1 = new google.visualization.PieChart(document.getElementById('team1-piechart'));
+    chart1.draw(team1ChartData, {})
+
+}
+
 async function getMatchDetails() {
     let params = new URLSearchParams(window.location.search)
     let id = params.get("id")
@@ -46,7 +75,12 @@ async function getMatchDetails() {
     let response = await fetch("http://127.0.0.1:5500/match/" + id, options)
     let responseData = await response.json()
 
-    console.log(responseData)
+    let team1Name = responseData.match.json_cruzamento.time[Object.keys(responseData.match.json_cruzamento.time)[0]].nome
+    let team2Name = responseData.match.json_cruzamento.time[Object.keys(responseData.match.json_cruzamento.time)[1]].nome
+
+    document.querySelector("#team1-outcome-title").textContent = `Desfechos ${team1Name}`
+
+    renderOutcomeCharts(responseData)
 }
 
 getMatchDetails()
