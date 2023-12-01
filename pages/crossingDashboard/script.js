77011dc105fcaed0b5fba438b2959cdc5122cdd6
DECO190 <deco9871@gmail.com>
2023-12-01 11:24:59 -0300

    feat: add google charts + get match req

--- pages/crossingDashboard/script.js
+++ pages/crossingDashboard/script.js
@@ -1,3 +1,5 @@
+google.charts.load('current', {'packages':['corechart']});
+
 document.querySelector(".username").textContent = `Olá, ${localStorage.getItem("club_name")}`
 
 function logout() {
@@ -12,3 +14,39 @@ function goToDefensiveDashboard() {
     window.location.href = "/pages/defensiveBreakthrough/defensiveBreakthrough.html?id=" + params.get("id")
 }
 
+function drawChart(data, elementId) {
+    var data = google.visualization.arrayToDataTable([
+      ...data
+    ]);
+
+    var options = {
+      title: 'My Daily Activities'
+    };
+
+    var chart = new google.visualization.PieChart(document.getElementById(elementId));
+
+    chart.draw(data, options);
+}
+
+async function getMatchDetails() {
+    let params = new URLSearchParams(window.location.search)
+    let id = params.get("id")
+
+    if (!id) {
+        return toast("Id inválido", "error")
+    }
+
+    let options = {
+        method: "GET",
+        headers: {
+            "Authorization": "Bearer " + localStorage.getItem("token")
+        }
+    }
+
+    let response = await fetch("http://127.0.0.1:5500/match/" + id, options)
+    let responseData = await response.json()
+
+    console.log(responseData)
+}
+
+getMatchDetails()
