728f3b4067ef9c9862e0376bab675835d1424a7d
MMp <mateusmoreirammp052@gmail.com>
2023-12-03 18:35:36 -0300

    coloquei as porcentagens nas zonas

--- pages/defensiveBreakthrough/script.js
+++ pages/defensiveBreakthrough/script.js
@@ -1,3 +1,4 @@
+localStorage.setItem('club_name','PAL')
 document.querySelector(".username").textContent = `Olá, ${localStorage.getItem("club_name")}`
 
 function logout() {
@@ -10,10 +11,6 @@ function goToCrossingDashboard() {
 
     window.location.href = "/pages/crossingDashboard/crossingDashboard.html?id=" + params.get("id")
 }
-
-// document.querySelector(".crossing-dash").onclick = function() {
-//     crossingDash() 
-// }
 
 const backendURL = 'http://127.0.0.1:5501/match/6564f369f82e76ba950789ab';
 
@@ -31,8 +28,26 @@ fetch(backendURL, {
     }
 })
 .then(data => {
-    var json_quebra = data["match"]["json_quebra"]
-    console.log(json_quebra)
+    var listaDeObjetos = Object.entries(data["match"]["json_quebra"]["time"]).map(([chave, valor]) => {
+        return { chave: chave, ...valor };
+      });
+
+    if (data["match"]["club_1_name"].toUpperCase() == localStorage.getItem('club_name').toUpperCase())
+        var json_quebra = listaDeObjetos["0"]
+    else
+        var json_quebra = listaDeObjetos["1"]
+
+    /////////////
+    
+    let zona1b = document.getElementById('1.B');
+    let zona1a = document.getElementById('1.A');
+    let zona3a = document.getElementById('3.A');
+    let zona3b = document.getElementById('3.B');
+
+zona1b.innerText = "1.B"+"\n"+~~(json_quebra["zonas"]["Zona 1 - B"]/json_quebra["rupturas"].length*100)+"%";
+zona1a.innerText = "1.A"+"\n"+~~(json_quebra["zonas"]["Zona 1 - A"]/json_quebra["rupturas"].length*100)+"%";
+zona3a.innerText = "3.A"+"\n"+~~(json_quebra["zonas"]["Zona 3 - A"]/json_quebra["rupturas"].length*100)+"%";
+zona3b.innerText = "3.B"+"\n"+~~(json_quebra["zonas"]["Zona 3 - B"]/json_quebra["rupturas"].length*100)+"%";
 })
 .catch(error => {
     console.error('Erro:', error);
